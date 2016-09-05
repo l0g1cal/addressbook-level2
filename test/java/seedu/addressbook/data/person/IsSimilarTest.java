3bc60428091db48bde02f718806f0fdd7caf29fd
l0g1cal <yongjiawern@gmail.com>
2016-09-06 03:19:22 +0800

    Add null test case

--- test/java/seedu/addressbook/data/person/IsSimilarTest.java
+++ test/java/seedu/addressbook/data/person/IsSimilarTest.java
@@ -1,14 +1,18 @@
 package seedu.addressbook.data.person;
 
 import static org.junit.Assert.*;
+import seedu.addressbook.data.person.Name;
+
+import seedu.addressbook.data.exception.IllegalValueException;
 
 import org.junit.Test;
 
 public class IsSimilarTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void otherNameNull() throws IllegalValueException{
+		Name name = new Name("JW");
+		assertEquals(name.isSimilar(null), false);
+	} 
 
 }
